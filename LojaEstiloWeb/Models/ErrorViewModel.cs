0123e1a381c6012954bc093632e413ff53e0d844
taynasuperti <taynasuperti@gmail.com>
2025-02-03 15:01:27 -0300

    Criado o projeto MVC

---
+++ LojaEstiloWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LojaEstiloWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
