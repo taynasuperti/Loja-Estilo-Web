ebede440aeb1ec6d052559573a072d70badd4952
taynasuperti <taynasuperti@gmail.com>
2025-02-06 16:49:43 -0300

    início da estilização da navbar

---
+++ Banco de Dados/ScriptDDL.sql
@@ -0,0 +1,34 @@
+--Criar banco
+CREATE DATABASE loja; --cria o banco de dados
+
+use loja; --coloca o banco em uso
+
+CREATE TABLE Categoria --cria as tabelas
+(
+	Id			int not null identity primary key, --faz o id ser automatico
+	Nome		varchar(50) not null, --nome da categoria
+	Foto		varchar(200), 
+	ExibirHome	bit 
+);
+
+CREATE TABLE Produto
+(
+	Id			int not null identity primary key,
+	Nome		varchar(100) not null, 
+	Descricao	varchar(1000),
+	CategoriaId	int not null,
+	ValorCusto	numeric(10,2),
+	ValorVenda	numeric(10,2) not null,
+	QTdeEstoque	int not null default 0,
+	Destaque	bit default 0,
+	foreign key	(CategoriaId) references Categoria(Id)
+);
+
+CREATE TABLE  ProdutoFoto
+(
+	Id			int not null identity primary key,
+	ProtudoId	int not null,
+	Foto		varchar(200) not null,
+	Descricao	varchar(100),
+	foreign key (ProdutoId) references Produto(Id)
+);
