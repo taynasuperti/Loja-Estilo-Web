3564315696f167310e4e8309a276ed6dc7d76cf8
taynasuperti <taynasuperti@gmail.com>
2025-03-24 16:15:38 -0300

    backup do banco de dados

--- LojaEstiloWeb/Controllers/CategoriasController.cs
+++ LojaEstiloWeb/Controllers/CategoriasController.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using LojaEstiloWeb.Data;
 using LojaEstiloWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 
 
 namespace LojaEstiloWeb.Controllers;
@@ -64,6 +66,89 @@ public class CategoriasController : Controller
             return RedirectToAction(nameof(Index));
         }
         return View(categoria);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int? id) 
+    {
+        if (id == null)
+            return NotFound();
+        var categoria = await _db.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        if (categoria == null)
+            return NotFound();
+        return View(categoria);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id) 
+    {
+        if (id == null)
+            return NotFound();
+        var categoria = await _db.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        if (categoria == null)
+            return NotFound();
+        return View(categoria);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Categoria categoria, IFormFile Arquivo)
+    {
+        if (id != categoria.Id)
+            return NotFound();
+        if (ModelState.IsValid)
+        {
+            try
+            {
+              if (Arquivo != null)
+            {
+                string nomeArquivo = categoria.Id + Path.GetExtension(Arquivo.FileName);
+                string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
+                string novoArquivo = Path.Combine(caminho, nomeArquivo);
+                using (FileStream stream = new FileStream(novoArquivo, FileMode.Create))
+                {
+                    await Arquivo.CopyToAsync(stream);
+                }
+
+                categoria.Foto = "\\img\\categorias\\" + nomeArquivo;
+
+                _db.Categorias.Update(categoria);
+                await _db.SaveChangesAsync();
+            }
+  
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+             TempData["Success"] = "Categoria alterada com sucesso!";
+            return RedirectToAction(nameof(Index));
+        }
+        return View(categoria);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id) 
+    {
+        if (id == null)
+            return NotFound();
+        var categoria = await _db.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        if (categoria == null)
+            return NotFound();
+        return View(categoria);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var categoria = await _db.Categorias.FindAsync(id);
+        if (categoria != null)
+            _db.Categorias.Remove(categoria);
+        await _db.SaveChangesAsync();
+        TempData["Sucess"] = "Categoria Excluída com Sucesso";
+        return RedirectToAction(nameof(Index));
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
